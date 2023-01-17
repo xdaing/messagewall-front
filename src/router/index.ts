e510045a1a8e8fe34e2a2d687f0dfff1e8fe1322
miemie <2380848997@qq.com>
2023-01-18 01:00:29 +0800

    feat:设置路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: Home,
+      redirect: 'message',
+      children: [
+        {
+          path: 'message',
+          component: () => import('@/views/Message.vue'),
+          meta: { title: '留言' }
+        },
+        {
+          path: 'photo',
+          component: () => import('@/views/Photo.vue'),
+          meta: { title: '照片' }
+        }
+      ]
+    }
+  ]
+})
+router.afterEach(to => {
+  document.title = `咩咩 | ${to.meta.title}`
 })
 
 export default router
