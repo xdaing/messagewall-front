014f9b96cbf257bad855babc247e82ec83b39f8b
miemie <2380848997@qq.com>
2023-01-18 01:00:45 +0800

    feat:样式引入

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from '@/App.vue'
 import router from '@/router'
 import store from '@/stores'
 import 'virtual:svg-icons-register'
+import '@/styles/main.scss'
 
 const app = createApp(App)
 
